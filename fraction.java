6ce2b4721ba8a335619fcce2e32540748f62b81c
Christian Nyl M. Pulmano <106977049+AGHusky@users.noreply.github.com>
2024-02-16 20:04:41 +0800

    Add files via upload

---
+++ fraction.java
@@ -0,0 +1,4 @@
+package prelimProject;
+
+public class fraction {
+}
---
+++ fractionArithmetic.java
@@ -0,0 +1,8 @@
+package prelimProject;
+
+public class fractionArithmetic {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Fraction Arithmetic!");
+        System.out.println("");
+    }
+}

2b42e32196dd58cbb114e88301ff4512a3c695e7
AJDT <156164974+thegixxer@users.noreply.github.com>
2024-02-17 12:16:45 +0800

    Update FractionClass.java

--- FractionClass.java
+++ FractionClass.java
@@ -1,1 +1,37 @@
+package GroupProj;
+
+import java.util.Scanner;
+
+public class Fraction {
+
+        private int[] numerator;
+        private int[] denominator;
+        private  int index;
+
+
+        public  Fraction(){
+            index = 0;
+
+
+        }
+       public Fraction(int index){
+            numerator = new int[index];
+            denominator = new int[index];
+            this.index = 0;
+
+        }
+        public Fraction(int[] n, int[] d){
+            numerator = n;
+            denominator = d;
+        }
+        public int getNumerator(int index){
+            return numerator[index];
+
+        }
+        public int getDenominator(int index){
+            return denominator[index];
+        }
+
+
+    }
 

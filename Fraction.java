fd15d80523b4d9bf018b2a142821f1110bcb81a7
Yesu_Desu <144157115+YesuDesu21@users.noreply.github.com>
2024-02-17 09:15:29 +0800

    -jas/trial

---
+++ Fraction.java
@@ -0,0 +1,30 @@
+public class Fraction {
+
+    private int numOne;
+    private int numTwo;
+    private int denomOne;
+    private int denomTwo;
+
+    public Fraction(){
+        numOne = 0; //nOne first numerator
+        numTwo = 0; //nTwo second numerator
+        denomOne = 0;//dOne first denominator
+        denomTwo = 0;//dTwo second denominator
+    }
+
+    public int getNumOne(int nOne){
+        return numOne;
+    }
+    public int getNumTwo(int nTwo){
+        return numOne;
+    }
+    public int getDenomOne(int dOne) {
+        return denomOne;
+    }
+    public int getDenomTwo(int dTwo){
+        return denomTwo;
+    }
+    public int subtraction(int d, int d){
+        if(d)
+    }
+}
---
+++ FractionArithmetic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FractionArithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Fraction solve = new Fraction();
+        int nOne, nTwo, dOne, dTwo;
+
+        System.out.println("Welcome to the Fraction Arithmetic!");
+        System.out.println("");
+
+
+
+
+
+
+    }
+}

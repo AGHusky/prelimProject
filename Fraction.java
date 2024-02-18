10bbf9ba6bed8be97abd9295bba88780a5f3afc1
Abrellie <160217104+Abrellie@users.noreply.github.com>
2024-02-18 22:49:54 +0800

    final - lopez
    final - lopez

--- Fraction.java
+++ Fraction.java
@@ -1,30 +1,89 @@
-public class Fraction {
+package prog2.prelimgroup;
 
-    private int numOne;
-    private int numTwo;
-    private int denomOne;
-    private int denomTwo;
+public class Fraction {
+    private int numerator;
+    private int denominator;
 
     public Fraction(){
-        numOne = 0; //nOne first numerator
-        numTwo = 0; //nTwo second numerator
-        denomOne = 0;//dOne first denominator
-        denomTwo = 0;//dTwo second denominator
+        numerator = 0;
+        denominator = 1;
+    }
+    public Fraction(int wholeNumVal){ //finished
+        numerator = wholeNumVal;
+        denominator = 1;
     }
 
-    public int getNumOne(int nOne){
-        return numOne;
+    public Fraction(int numerator, int denominator){ //finished
+        this.numerator = numerator;
+        this.denominator = denominator;
+        reduceFraction();
+
     }
-    public int getNumTwo(int nTwo){
-        return numOne;
+    public void setNumerator(int num){ //finished
+        numerator = num;
     }
-    public int getDenomOne(int dOne) {
-        return denomOne;
+
+    public void setDenominator(int den){ //finished
+        denominator = den;
     }
-    public int getDenomTwo(int dTwo){
-        return denomTwo;
+    public int getDenominator(){ //finished
+        return denominator;
     }
-    public int subtraction(int d, int d){
-        if(d)
+
+    public String toString() { //todo
+        return (numerator + "/" + denominator + " or " + toDouble());
+    }
+    public double toDouble(){ //finished
+        return ((double) numerator / denominator);
+    }
+    private int computeGCD(){
+        return 15;
+    }
+
+    public void addFraction(int[] num, int[] den){ // - Lopez
+        int fnum, fden, firstNum, secNum;
+        if (den[0] == den[1]) {             // ---if same denominators---
+            fnum = num[0] + num[1];         // add the numerators
+            fden = den[0];                  // copy denominator
+            System.out.println(fnum + "/" + fden);
+        } else if (den[0] != den[1]) {      // ---if diff denominators---
+            firstNum = den[1] * num[0];     // compute first numerator
+            secNum= den[0] * num[1];        // compute second numerator
+            fnum = firstNum + secNum;       // add the numerators
+            fden = computeGCD();            // gcd of the denominators will be the final denominator
+            //print
+            System.out.println("Answer: " + fnum + "/" + fden);
+        }
+    }
+    public void subtractFraction(int[] num, int[] den){
+        //Fraction 1 =  + num[0] +"/"+den[0]
+        //Fraction 2 =  + num[1] +"/"+den[1]
+        //gcd = to be used in formula
+        int temp; //to be used for multiplying to numerator
+        int diff; //difference
+        int gcd = computeGCD();
+        setDenominator(gcd); // temporary value of gcd because gcd method is not finished (DELETE AFTER)
+        //sample input (4/5 - 2/3)
+        temp = gcd/den[0];
+        num[0] = temp * num[0];
+        temp = gcd/den[1];
+        num[1] = temp * num[1];
+        diff = num[0]-num[1];
+        setNumerator(diff);
+    }
+
+    public int multiplyFraction(){
+        //todo
+        return 0;
+    }
+
+    public int divideFraction(){
+        //todo
+        return 0;
+    }
+
+    public int reduceFraction(){
+        //todo
+        return 0;
     }
 }
--- FractionArithmetic.java
+++ FractionArithmetic.java
@@ -1,18 +1,109 @@
+package prog2.prelimgroup;
+
 import java.util.Scanner;
 
 public class FractionArithmetic {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Fraction solve = new Fraction();
-        int nOne, nTwo, dOne, dTwo;
+        Scanner kbd = new Scanner(System.in);
+        Fraction users = new Fraction();
+        int[] num = new int[3];
+        int[] den = new int[3];
+        int userinput = 0;
 
-        System.out.println("Welcome to the Fraction Arithmetic!");
-        System.out.println("");
-
-
-
+        //alwin part greetings
 
 
 
+
+        //
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Fractions!");
+        System.out.println();
+
+        //check the user input
+        do {
+            userinput = AskUser();
+            switch (userinput){
+                case 1: //enter value of Fraction 1
+                    System.out.println("Enter the value for Fraction 1");
+                    System.out.print("numerator: ");
+                    num[0] = kbd.nextInt();
+                    users.setNumerator(num[0]);
+                    do{
+                        System.out.print("denominator: ");
+                        den[0] = kbd.nextInt();
+                    }while (den[0] == 0);
+                    users.setDenominator(den[0]);
+                    System.out.println("Fraction 1 = " + num[0] +"/"+den[0]);
+                    System.out.printf("Decimal = %.2f", users.toDouble());
+                    System.out.println();
+                    System.out.println();
+                    break;
+
+                case 2: //enter value of Fraction 2
+                    System.out.println("Enter the value for Fraction 2");
+                    System.out.print("numerator: ");
+                    num[1] = kbd.nextInt();
+                    users.setNumerator(num[1]);
+                    do{
+                        System.out.print("denominator: ");
+                        den[1] = kbd.nextInt();
+                    }while (den[1] == 0);
+                    users.setDenominator(den[1]);
+                    System.out.println("Fraction 1 = " + num[1] +"/"+den[1]);
+                    System.out.printf("Decimal = %.2f", users.toDouble());
+                    System.out.println();
+                    System.out.println();
+                    break;
+
+                case 3: //addition - Lopez
+                    //getting the gcd
+                    System.out.println("The sum of " + num[0] +"/"+den[0] + " and " + num[1] +"/"+den[1] + " is: ");
+                    users.addFraction(num, den);
+                    break;
+
+                case 4: //subtraction - Doria
+                    Fraction sub = new Fraction();
+                    int diff;
+                    System.out.println("Difference of " + num[0] +"/"+den[0] + " and " + num[1] +"/"+den[1] + ": ");
+                    sub.subtractFraction(num, den);
+                    System.out.println("result is: " + sub.toString());
+                    break;
+
+
+                case 5: //multiplication - garcia
+
+                case 6: //divide - Vigilia
+
+                case 7: // reduce - Pulmano
+
+                case 8: // quit program
+                    exit();
+
+                default:
+                    System.out.println("User input is out of bound!");
+                    System.out.println("TRY AGAIN!");
+                    AskUser();
+            }
+        }while (true);
+    }
+
+    public static int AskUser(){
+        String[] ask = {"Enter Value of Fraction 1", "Enter Value of Fraction 2","Add Fractions", "Subtract Fractions", "Multiply Fractions", "Divide Fractions", "Reduce Fractions", "Quit",};
+        Scanner kbd = new Scanner(System.in);
+        System.out.println("What do you want to do? ");
+        for (int i = 0; i < ask.length; i++){
+            System.out.println((i + 1) + ". " + ask[i]);
+        }
+        System.out.print("UserInput: ");
+        return kbd.nextInt();
+    }
+
+    public static void exit(){
+        //ending UI - Doria
+
+
+
+        System.exit(0);
     }
 }
